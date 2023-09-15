d59ed0971d0ef83d9fd26f4363a2531c10c1b0d0
Anurag Pandey <arushpandey123buy@gmail.com>
2023-09-15 22:18:44 +0530

    ok

--- src/pages/congartulation/index.jsx
+++ src/pages/congartulation/index.jsx
@@ -1,13 +1,33 @@
 import React from "react";
-
+import Cong from "../../images/Confetti.png";
 function Congartulation() {
   return (
     <>
       <div className="congartulation-big-box">
-        <div className="side-logo-box"></div>
-        <div className="congartulation-box"></div>
-        <div className="congartulation-hobbies-box"></div>
-        <div className="proceed-button-box"></div>
+        <div className="side-logo-box">MARG</div>
+        <div className="congartulation-box">
+          <div className="congaratulation-box-img">
+            <img src={Cong} alt="" />
+          </div>
+          <div className="congaratulation-text">Congaratulation</div>
+          <br />
+
+          <div className="congaratulation-sub-hedding">
+            You have cleared your test.
+          </div>
+        </div>
+        <br />
+        <br />
+        <div className="congartulation-hobbies-box">
+          <p>Select your intrests/Hobbies</p>
+          <br />
+
+          <form action="intrest" className="form-for-intest">
+            <input type="text" />
+            <button type="submit">Proceed</button>
+          </form>
+        </div>
+        
       </div>
     </>
   );
